c423d48a6de856bed4b157a6a969dcda7ab22df3
Audric Le Meitour <audriclemeitour@gmail.com>
2018-12-13 15:28:27 +0100

    Formulaire accueil fonctionnel; Audric Le Meitour

--- app/src/main/java/nantes/iut/lutinsnantais/Liste.java
+++ app/src/main/java/nantes/iut/lutinsnantais/Liste.java
@@ -7,7 +7,7 @@ import android.widget.Toast;
 
 import com.android.volley.*;
 
-import com.android.volley.toolbox.*;
+//import com.android.volley.toolbox.*;
 
 import org.json.JSONObject;
 
@@ -16,19 +16,19 @@ public class Liste extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*
+
         String sexe = getIntent().getStringExtra("sexe");
         String prenom = getIntent().getStringExtra("prenom");
-        int annee_naissance_int = getIntent().getIntExtra("anne_naissance", 0);
+        int annee_naissance_int = getIntent().getIntExtra("annee_naissance", 0);
         String annee_naissance = String.valueOf(annee_naissance_int);
-        */
+
         // Récupération des données
 
         String url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes";
 
-        String sexe = "GARCON";
-        String prenom = "julia";
-        String annee_naissance = "2016";
+       // String sexe = "GARCON";
+       // String prenom = "julia";
+       // String annee_naissance = "2016";
 
 
         if (!sexe.isEmpty())
@@ -36,7 +36,7 @@ public class Liste extends AppCompatActivity {
         if (!prenom.isEmpty())
             url += "&refine.prenom=" + prenom;
         if (!annee_naissance.isEmpty())
-            url += "&refine.anne_naissance=" + annee_naissance;
+            url += "&refine.annee_naissance=" + annee_naissance;
 
         String TAG = Liste.class.getSimpleName();
         Log.d(TAG, "URL : " + url);
