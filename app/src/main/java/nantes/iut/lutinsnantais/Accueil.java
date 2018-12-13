5ba3ab66b4cb0c0cd6cbfb7ba112e7b119e6ccc9
Audric Le Meitour <audriclemeitour@gmail.com>
2018-12-13 15:24:20 +0100

    Formulaire Accueuil fonctionnel ; Audric Le Meitour

--- app/src/main/java/nantes/iut/lutinsnantais/Accueil.java
+++ app/src/main/java/nantes/iut/lutinsnantais/Accueil.java
@@ -1,7 +1,11 @@
 package nantes.iut.lutinsnantais;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Accueil extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class Accueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+        Button btnValider = findViewById(R.id.valider);
+        btnValider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Accueil.this, Liste.class);
+                EditText tPrenom = findViewById(R.id.txtPrenom);
+                EditText tSexe = findViewById(R.id.txtSexe);
+                EditText tNaissance = findViewById(R.id.txtNaissance);
+
+                String prenom = tPrenom.getText().toString();
+                String sexe = tSexe.getText().toString();
+                String tempNaissance = tNaissance.getText().toString();
+                int annee_naissance = Integer.parseInt(tempNaissance);
+
+                i.putExtra("prenom",prenom);
+                i.putExtra("sexe",sexe);
+                i.putExtra("annee_naissance", annee_naissance);
+
+                startActivity(i);
+            }
+        });
     }
+
+
+
 }
