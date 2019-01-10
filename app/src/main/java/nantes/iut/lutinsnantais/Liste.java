ba24bc8f459cf73862a9316711f379ba2f651f16
aubinGaboriau <magicauau12@gmail.com>
2019-01-10 20:17:25 +0100

    Activité liste (3)

--- app/src/main/java/nantes/iut/lutinsnantais/Liste.java
+++ app/src/main/java/nantes/iut/lutinsnantais/Liste.java
@@ -24,18 +24,39 @@ import bean.Enfant;
 
 public class Liste extends AppCompatActivity {
 
+    private ListView lv;
+    private ArrayAdapter<String> arrayAdapter;
+    private List<String> enfantsPrenom = new ArrayList<String>();
+    private List<Enfant> listeEnfants = new ArrayList<Enfant>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste);
+
+        // Récupération des données
+
+        String TAG = Liste.class.getSimpleName();
+
+        lv = (ListView) findViewById(R.id.listEnfant);
+
+        arrayAdapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1
+        );
+
+        lv.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        String url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes";
 
         String sexe = getIntent().getStringExtra("sexe");
         String prenom = getIntent().getStringExtra("prenom");
         String annee_naissance = getIntent().getStringExtra("annee_naissance");
-
-        // Récupération des données
-
-        String url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes";
-
 
         if (!sexe.isEmpty())
             url += "&refine.sexe=" + sexe;
@@ -44,63 +65,45 @@ public class Liste extends AppCompatActivity {
         if (!annee_naissance.isEmpty())
             url += "&refine.annee_naissance=" + annee_naissance;
 
-        String TAG = Liste.class.getSimpleName();
-        Log.d(TAG, "URL : " + url);
-
-
-        final ArrayList<Enfant> listeEnfants = new ArrayList<Enfant>();
-
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
 
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
+                    try {
 
-                            String TAG = Liste.class.getSimpleName();
-
-                            JSONArray enfants = response.getJSONArray("records");
-
-                            Log.d(TAG, "array enfants retrieved");
-
-                            for (int i = 0; i < enfants.length(); i++) {
-                                int an = Integer.parseInt(enfants.getJSONObject(i).getJSONObject("fields").getString("annee_naissance"));
-                                String p = enfants.getJSONObject(i).getJSONObject("fields").getString("prenom");
-                                String s = enfants.getJSONObject(i).getJSONObject("fields").getString("sexe");
-                                listeEnfants.add(new Enfant(p, s, an, true));
-                            }
-
-                            Log.d(TAG, "liste enfants created");
-
-                            List<String> enfantsPrenom = new ArrayList<String>();
-
-                            for (Enfant e : listeEnfants)
-                                enfantsPrenom.add(e.getPrenom());
-
-                            Log.d(TAG, "liste enfantPrenom created");
-
-                            ListView lv = (ListView) findViewById(R.id.listEnfant);
-
-                            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                                    getApplicationContext(),
-                                    android.R.layout.simple_list_item_1,
-                                    enfantsPrenom
-                            );
-
-                            Log.d(TAG, "array adapter created");
-                            setContentView(R.layout.activity_liste);
-
-                        } catch (JSONException e) {
-
-                        }
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
                         String TAG = Liste.class.getSimpleName();
-                        Log.e(TAG, "Error retrieving kids informations");
+
+                        JSONArray enfants = response.getJSONArray("records");
+
+                        for (int i = 0; i < enfants.length(); i++) {
+                            int an = Integer.parseInt(enfants.getJSONObject(i).getJSONObject("fields").getString("annee_naissance"));
+                            String p = enfants.getJSONObject(i).getJSONObject("fields").getString("prenom");
+                            String s = enfants.getJSONObject(i).getJSONObject("fields").getString("sexe");
+                            listeEnfants.add(new Enfant(p, s, an, true));
+                        }
+
+                        for (Enfant e : listeEnfants)
+                            enfantsPrenom.add(e.getPrenom());
+
+                        Log.d(TAG, "onResponse: enfantPrenom created");
+                        arrayAdapter.addAll(enfantsPrenom);
+                        arrayAdapter.notifyDataSetChanged();
+                        Log.d(TAG, "onResponse: arrayAdapterUpdated created");
+
+                    } catch (JSONException e) {
+
                     }
-                });
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    String TAG = Liste.class.getSimpleName();
+                    Log.e(TAG, "Error retrieving kids informations");
+                }
+            }
+        );
+
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
     }
 }
